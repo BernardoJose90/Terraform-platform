3d4e5a733a9a75fdbb539ceffb587124c8c3d712
bernardo jose <bernardo.jose@bernardos-MacBook-Pro.local>
2026-07-08 02:15:38 +0100

    feat: add VPC configuration

--- member-accounts/network/main.tf
+++ member-accounts/network/main.tf
@@ -45,11 +45,11 @@ provider "aws" {
 
 }
 
-# module "vpc" {
-#   source = "../../modules/vpc"
-#   name   = "network-vpc"
-#   cidr   = "10.1.0.0/16"
-# }
+module "vpc" {
+ source = "../../modules/vpc"
+ name   = "network-vpc"
+ cidr   = "10.1.0.0/16"
+ }
 
 module "terraform_deploy_role" {
   source       = "../../modules/terraform-deploy-role"
