beae3de189fe2970965d97c8720e8efb2d0d133b
bernardo jose <bernardo.jose@Mac.home>
2026-07-14 14:07:27 +0100

    update/removed-production-vpc

--- member-accounts/production/main.tf
+++ member-accounts/production/main.tf
@@ -44,11 +44,7 @@ provider "aws" {
 
 }
 
-module "vpc" {
-  source = "../../modules/vpc"
-  name   = "prod-vpc"
-  cidr   = "10.40.0.0/16"
-}
+
 
 module "terraform_deploy_role" {
   source       = "../../modules/terraform-deploy-role"
