bd44ed5eef6ce977af5c8ed4be98d214f9431dcb
bernardo jose <bernardo.jose@bernardos-MacBook-Pro.local>
2026-07-20 23:50:03 +0100

    updated VPC module format

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,7 +19,7 @@ module "vpc" {
   # false + an empty public_subnets list gives you a fully private VPC.
 
   tags = var.tags
-
+  
 }
 
 # Extra "spoke egress" route added to every private route table, pointing
@@ -32,5 +32,5 @@ resource "aws_route" "private_to_tgw" {
   route_table_id         = each.value
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = var.tgw_id
-  
+
 }
