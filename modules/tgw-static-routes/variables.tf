4cfa70bc02657eed995c92c1388fffdfd255aada
bernardo jose <bernardo.jose@Mac.home>
2026-08-10 22:13:49 +0100

    feat(tgw-static-routes): add blackhole route support

--- modules/tgw-static-routes/variables.tf
+++ modules/tgw-static-routes/variables.tf
@@ -5,4 +5,11 @@ variable "tgw_route_table_id" {
 variable "routes" {
   description = "Map of destination CIDR -> transit gateway attachment ID"
   type        = map(string)
+  default     = {}
+}
+
+variable "blackhole_cidrs" {
+  description = "Destination CIDRs to explicitly drop rather than route anywhere. Wins over a broader route (e.g. 0.0.0.0/0) to the same table via longest-prefix-match."
+  type        = list(string)
+  default     = []
 }
