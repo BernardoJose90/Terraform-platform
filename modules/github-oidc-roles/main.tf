24b1ebe59d9447d8cce044a6db6c52a79a6a640d
bernardo jose <bernardo.jose@websummit.com>
2026-08-13 19:01:08 +0100

    fixed fmt issue

--- modules/github-oidc-roles/main.tf
+++ modules/github-oidc-roles/main.tf
@@ -316,9 +316,9 @@ data "aws_iam_policy_document" "github_oidc_trust_plan" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-              "repo:${var.github_org}/${var.github_repo}:pull_request",
-              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
-            ]
+        "repo:${var.github_org}/${var.github_repo}:pull_request",
+        "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
+      ]
     }
   }
 }
